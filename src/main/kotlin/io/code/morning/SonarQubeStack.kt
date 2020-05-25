a822c18b54b22888f24a8498365664706e732056
otajisan <taji.taji.otajisan@gmail.com>
2020-05-25 16:46:12 +0900

    :+1: add tagging

--- src/main/kotlin/io/code/morning/SonarQubeStack.kt
+++ src/main/kotlin/io/code/morning/SonarQubeStack.kt
@@ -24,13 +24,95 @@ class SonarQubeStack @JvmOverloads constructor(app: App, id: String, props: Stac
     // VPC
     val vpc = Vpc(
         this, "vpc", VpcProps.builder()
+        .enableDnsHostnames(true)
+        .enableDnsSupport(true)
         .maxAzs(2)
         .cidr("10.0.0.0/16")
+        .subnetConfiguration(
+            mutableListOf(
+                SubnetConfiguration.builder()
+                    .cidrMask(18)
+                    .name("Public")
+                    .subnetType(SubnetType.PUBLIC)
+                    .build(),
+                SubnetConfiguration.builder()
+                    .cidrMask(18)
+                    .name("Private")
+                    .subnetType(SubnetType.PRIVATE)
+                    .build()
+            )
+        )
         .build()
     )
 
+    // Security Group settings
+    /*
+    val albSecurityGroup = SecurityGroup(this, "sonar-alb-sg", SecurityGroupProps.builder()
+        .securityGroupName("sonar-alb-sg")
+        .description("security group for SonarQube ALB")
+        .allowAllOutbound(true)
+        .vpc(vpc)
+        .build()
+    )
+    albSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(80), "Http Port", false)
+
+    val ecsSecurityGroup = SecurityGroup(this, "sonar-sg", SecurityGroupProps.builder()
+        .securityGroupName("sonar-sg")
+        .description("security group for SonarQube ECS")
+        .allowAllOutbound(true)
+        .vpc(vpc)
+        .build()
+    )
+    ecsSecurityGroup.connections.allowFrom(
+        Connections(ConnectionsProps.builder()
+            .securityGroups(listOf(albSecurityGroup))
+            .build()
+        ),
+        Port.tcp(80),
+        "ECS Port"
+    )
+
+    val rdsSecurityGroup = SecurityGroup(this, "sonar-db-sg", SecurityGroupProps.builder()
+        .securityGroupName("sonar-db-sg")
+        .description("security group for SonarQube RDS")
+        .vpc(vpc)
+        .allowAllOutbound(true)
+        .build()
+    )
+    rdsSecurityGroup.connections.allowFrom(
+        Connections(ConnectionsProps.builder()
+            .securityGroups(listOf(ecsSecurityGroup))
+            .build()
+        ),
+        Port.tcp(5432),
+        "DB Port"
+    )
+
+     */
+
+    val rdsSecurityGroup = SecurityGroup(this, "sonar-db-sg", SecurityGroupProps.builder()
+        .securityGroupName("sonar-db-sg")
+        .description("security group for SonarQube RDS")
+        .vpc(vpc)
+        .allowAllOutbound(true)
+        .build()
+    )
+
+    rdsSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(5432), "SonarQube RDS Port", false)
+
+    /*
+    rdsSecurityGroup.connections.allowFrom(
+        Connections(ConnectionsProps.builder()
+            .build()
+        ),
+        Port.tcp(5432),
+        "SonarQube RDS Port"
+    )
+     */
+
     // RDS - PostgreSQL
     val databasePassword: String = StringParameter.valueFromLookup(this, "SONAR_JDBC_PASSWORD")
+
     val rds = DatabaseInstance(this, "sonar-db", DatabaseInstanceProps.builder()
         .instanceIdentifier("sonar-db")
         //.timezone("Asia/Tokyo")
@@ -39,6 +121,8 @@ class SonarQubeStack @JvmOverloads constructor(app: App, id: String, props: Stac
         .engine(DatabaseInstanceEngine.POSTGRES)
         .instanceClass(InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.MICRO))
         .vpc(vpc)
+        .securityGroups(listOf(rdsSecurityGroup))
+        .enablePerformanceInsights(true)
         .autoMinorVersionUpgrade(true)
         .databaseName("sonar")
         .multiAz(false)
@@ -47,11 +131,6 @@ class SonarQubeStack @JvmOverloads constructor(app: App, id: String, props: Stac
         .build()
     )
 
-    val rdsSecurityGroup = SecurityGroup(this, "sonar-db-sg", SecurityGroupProps.builder()
-        .securityGroupName("sonar-db-sg")
-        .vpc(vpc)
-        .build()
-    )
     rds.connections.allowDefaultPortFrom(rdsSecurityGroup)
 
     // ECS Cluster
@@ -76,13 +155,14 @@ class SonarQubeStack @JvmOverloads constructor(app: App, id: String, props: Stac
         .instanceType(InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.MEDIUM))
         .desiredCapacity(1)
         .maxCapacity(1)
-        .minCapacity(1)
+        .minCapacity(0)
         .vpc(vpc)
         .machineImage(EcsOptimizedImage.amazonLinux2())
         .keyName("sonar-key")
         .userData(userData)
         .build()
     )
+    //asg.addSecurityGroup(ecsSecurityGroup)
 
     ecsCluster.addAutoScalingGroup(asg)
 
@@ -141,7 +221,7 @@ class SonarQubeStack @JvmOverloads constructor(app: App, id: String, props: Stac
         ApplicationLoadBalancedEc2ServiceProps.builder()
             .cluster(ecsCluster)
             .serviceName("sonarqube-service")
-            // TODO: set Route53
+            // TODO: to be set Route53
             //.domainName("sonar.morningcode.io")
             .desiredCount(1)
             .taskDefinition(taskDefinition)
@@ -149,6 +229,8 @@ class SonarQubeStack @JvmOverloads constructor(app: App, id: String, props: Stac
             .publicLoadBalancer(true)
             .build()
     )
+
+    //ec2Service.loadBalancer.connections.addSecurityGroup(albSecurityGroup)
 
     // Cloud Map
     val namespace = PrivateDnsNamespace(
@@ -163,6 +245,7 @@ class SonarQubeStack @JvmOverloads constructor(app: App, id: String, props: Stac
     val service = namespace.createService(
         "service",
         ServiceProps.builder()
+            .name("sonarqube-service")
             .namespace(namespace)
             .dnsRecordType(DnsRecordType.A_AAAA)
             .dnsTtl(Duration.seconds(30))
@@ -171,5 +254,8 @@ class SonarQubeStack @JvmOverloads constructor(app: App, id: String, props: Stac
     )
 
     service.registerLoadBalancer("load-balancer", ec2Service.loadBalancer)
+
+    // tagging
+    Tag.add(this, "ServiceName", "sonarqube")
   }
 }
